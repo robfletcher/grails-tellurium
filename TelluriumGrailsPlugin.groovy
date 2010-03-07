3a75309f456b118a16ebb4977c2cc914e5195dfb
Rob Fletcher <robert.w.fletcher@gmail.com>
2010-03-07 13:09:44 +0000

    plugin descriptor

--- TelluriumGrailsPlugin.groovy
+++ TelluriumGrailsPlugin.groovy
@@ -1,50 +1,36 @@
 class TelluriumGrailsPlugin {
-    // the plugin version
-    def version = "0.1"
-    // the version or versions of Grails the plugin is designed for
+
+    def version = "1.0-SNAPSHOT"
     def grailsVersion = "1.2.1 > *"
-    // the other plugins this plugin depends on
     def dependsOn = [:]
-    // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
     ]
 
-    // TODO Fill in these fields
-    def author = "Your name"
-    def authorEmail = ""
-    def title = "Plugin summary/headline"
+    def author = "Rob Fletcher"
+    def authorEmail = "rob@energizedwork.com"
+    def title = "Tellurium Plugin"
     def description = '''\\
-Brief description of the plugin.
+Integrates the Tellurium testing framework into Grails' functional test phase.
 '''
 
-    // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/tellurium"
 
     def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before 
     }
 
     def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
     }
 
     def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
     }
 
     def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
     }
 
     def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
     }
 
     def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
     }
 }
